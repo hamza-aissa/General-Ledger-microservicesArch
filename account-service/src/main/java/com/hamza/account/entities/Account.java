38d0263ae365ad5df579bdf326aad5c754cadb84
hamza-aissa <professional.aissa@gmail.com>
2025-10-30 00:26:45 +0100

    final secition 2 fixes

--- account-service/src/main/java/com/hamza/account/entities/Account.java
+++ account-service/src/main/java/com/hamza/account/entities/Account.java
@@ -3,8 +3,10 @@ package com.hamza.account.entities;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
