ed7604559c280227abd2dada74040a7fb0f9813e
hamza-aissa <professional.aissa@gmail.com>
2025-11-17 23:36:51 +0100

    added eurekaserver discorvery and registry service also added multi instance support with jar

--- transaction-service/src/main/java/com/hamza/transaction/service/ApiClient.java
+++ transaction-service/src/main/java/com/hamza/transaction/service/ApiClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.hamza.transaction.dto.AccountDto;
 
-@FeignClient(url = "http://localhost:8080", value = "ACCOUNT")
+@FeignClient(name = "ACCOUNT-SERVICE")
 public interface ApiClient {
 
   @GetMapping("api/accounts/{number}")
