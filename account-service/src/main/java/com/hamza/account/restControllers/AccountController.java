e713c02c8a77bbcf46992d9c485ac50377f62f82
hamza-aissa <professional.aissa@gmail.com>
2025-11-19 23:37:30 +0100

    section 6

--- account-service/src/main/java/com/hamza/account/restControllers/AccountController.java
+++ account-service/src/main/java/com/hamza/account/restControllers/AccountController.java
@@ -1,5 +1,7 @@
 package com.hamza.account.restControllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,18 +11,34 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hamza.account.dto.AccountDto;
 import com.hamza.account.service.AccountService;
-
-import lombok.AllArgsConstructor;
+import com.hamza.account.config.Configuration;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/accounts")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class AccountController {
-  private AccountService accountService;
+
+  private final AccountService accountService;
+
+  @Value("${build.version}")
+  private String buildVersion;
+
+  private final Configuration configuration;
 
   @GetMapping("/{number}")
   public ResponseEntity<AccountDto> getAccByNumber(@PathVariable("number") String number) {
-    return new ResponseEntity<AccountDto>(
-        accountService.getAccountByNumber(number), HttpStatus.OK);
+    return new ResponseEntity<>(accountService.getAccountByNumber(number), HttpStatus.OK);
+  }
+
+  @GetMapping("/version")
+  public ResponseEntity<String> version() {
+    return ResponseEntity.ok(buildVersion);
+  }
+
+  @GetMapping("/author")
+  public ResponseEntity<String> retrieveAuthorInfo() {
+    return ResponseEntity.ok(
+        configuration.getName() + " " + configuration.getEmail());
   }
 }
